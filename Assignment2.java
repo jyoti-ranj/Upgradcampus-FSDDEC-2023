79793b8b8af14358740e8240ebbf14af60783996
Jyoti Ranjan Sethi <jrs15jan2000@gmail.com>
2024-01-12 13:04:37 +0530

    2nd commit for Assignment2

--- Assignment2.java
+++ Assignment2.java
@@ -1,9 +1,16 @@
 class Assignment2 {
      public static void main(String[] args) {
+        String student_name="RAHUL";
         int student_roll=15;
         float student_marks=83.5f;
+        long student_phone_no=6370761219l;
+        double student_fees=20908.45;
+    System.out.println("student Name "+student_name);
     System.out.println("student rollNum "+student_roll); 
-    System.out.println("Student mark is "+student_marks);   
+    System.out.println("Student mark is "+student_marks);
+    System.out.println("This is student phone num "+student_phone_no);
+    System.out.println("Student fees per month "+student_fees);
+
         
     }
 
