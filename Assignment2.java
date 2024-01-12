0135e3a54dddff8394eb21e989a299dc905d3695
Jyoti Ranjan Sethi <jrs15jan2000@gmail.com>
2024-01-12 12:03:00 +0530

    2nd assignment commit

---
+++ Assignment2.java
@@ -0,0 +1,10 @@
+class Assignment2 {
+     public static void main(String[] args) {
+        int student_roll=15;
+        float student_marks=83.5f;
+    System.out.println("student rollNum "+student_roll); 
+    System.out.println("Student mark is "+student_marks);   
+        
+    }
+
+}
