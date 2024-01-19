779d15931134dd056cd70076b07d63536503b554
Jyoti Ranjan Sethi <jrs15jan2000@gmail.com>
2024-01-19 20:28:28 +0530

    Assignment 3.1 commit

---
+++ Assignment3_1.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Assignment3_1 {
+    public static void main(String[] args) {
+        System.out.println("How many times you want to Print");
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for(int i=1 ; i<=n; i++){
+            System.out.println("Java is Best");
+
+        }
+    }
+
+    
+}
