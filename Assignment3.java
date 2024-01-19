aaf981ec6de5e2e8a78e2626e95271dd1b5a00a3
Jyoti Ranjan Sethi <jrs15jan2000@gmail.com>
2024-01-19 20:35:42 +0530

    3rd Assignment commit

--- Assignment3.java
+++ Assignment3.java
@@ -4,14 +4,15 @@ public class Assignment3 {
     
     public static void main(String[] args) {
      System.out.println("What Is you Age");
-     Scanner sc = new Scanner(System.in);
-     int age= sc.nextInt();   
-    
+     try (Scanner sc = new Scanner(System.in)) {
+        int age= sc.nextInt();   
+        
 
-    if(age>=18){
-        System.out.println("You can Apply for Driving Licence");
-    }else{
-        System.out.println("You can't apply for Driving Licence");
+        if(age>=18){
+            System.out.println("You can Apply for Driving Licence");
+        }else{
+            System.out.println("You can't apply for Driving Licence");
+        }
     }
   }
 } 
