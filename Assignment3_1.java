a467c505e388b25956aed44ac35893193450e6a5
Jyoti Ranjan Sethi <jrs15jan2000@gmail.com>
2024-01-19 20:39:30 +0530

    Assignment 3(1) Commit

--- Assignment3_1.java
+++ Assignment3_1.java
@@ -2,11 +2,12 @@ import java.util.Scanner;
 public class Assignment3_1 {
     public static void main(String[] args) {
         System.out.println("How many times you want to Print");
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        for(int i=1 ; i<=n; i++){
-            System.out.println("Java is Best");
+        try (Scanner sc = new Scanner(System.in)) {
+            int n = sc.nextInt();
+            for(int i=1 ; i<=n; i++){
+                System.out.println("Java is Best");
 
+            }
         }
     }
 
